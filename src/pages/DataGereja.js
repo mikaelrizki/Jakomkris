e7929df624f5685da05faa5ac0b49a11227d1f2e
Azriel Setyo <86305687+Stay2607@users.noreply.github.com>
2023-08-11 23:05:00 +0700

    Revert "Update Data Gereja"

--- src/pages/DataGereja.js
+++ src/pages/DataGereja.js
@@ -1,31 +1,15 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { isTokenExpired } from '../utils/Utils'
+import { Link } from 'react-router-dom'
 import NavBar from '../components/navbar/NavBar';
-import Gereja from '../components/gereja/Gereja'
-import Footer from '../components/footer/Footer';
 
 const DataGereja = () => {
-    if (isTokenExpired()) {
-        sessionStorage.removeItem('token');
-        sessionStorage.removeItem('tokenExpiration');
-        console.log('Token has expired');
-    }
-
-    const token = sessionStorage.getItem('token');
-    if (token) {
-        return (
-            <div>
-                <NavBar />
-                <Gereja />
-                <Footer />
-            </div>
-        );
-    } else {
-        return (
-            <Navigate to="/login" replace={true} />
-        )
-    }
-}
+    return (
+        <div>
+            <NavBar />
+            <h1>Welcome to the Home Page</h1>
+            <Link to="/">Go to Gereja Page</Link>
+        </div>
+    );
+};
 
 export default DataGereja
