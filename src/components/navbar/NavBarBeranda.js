20b826da3c0d0b14b9c8109441a8c41cc9a44df1
mikaelrizki <mikael.rizki.pe@gmail.com>
2023-08-12 06:33:20 +0700

    Update Beranda

--- src/components/navbar/NavBarBeranda.js
+++ src/components/navbar/NavBarBeranda.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styleNavBarBeranda.css'
+import NavBar from './NavBar'
 
 const NavBarBeranda = () => {
+    const [isNavBarVisible, setIsNavBarVisible] = useState(false);
+
+    const toggleNavBar = () => {
+        setIsNavBarVisible(prevState => !prevState);
+    };
+
     return (
-        <div id='showNavBar'></div>
-    )
+        <div>
+            <div id='expandMore' onClick={toggleNavBar}>
+                <span className="material-symbols-outlined">
+                    {isNavBarVisible ? "expand_less" : "expand_more"}
+                </span>
+            </div>
+            <div id="navBarContainer">
+                {isNavBarVisible && <NavBar />}
+            </div>
+        </div>
+    );
 }
 
 export default NavBarBeranda
