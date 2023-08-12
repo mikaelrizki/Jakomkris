e795b4adff4c13f5ddddedcc5f8ffed002232ac5
mikaelrizki <mikael.rizki.pe@gmail.com>
2023-08-12 07:50:48 +0700

    Repair

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -15,9 +15,6 @@ const Maps = () => {
                         <div id="next" className="btnright"><img src={IconMove} width="20px" height="20px" alt="" aria-hidden="true" draggable="false" /></div> 
                     </a>
                 </div>
-            </div> 
-            <div id="gmaps">
-                <iframe title='Google Maps Dummy' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2155.4031794659472!2d110.37702260283012!3d-7.78696807133443!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5828f6fb7ef7%3A0xb710cc588f40770e!2sUniversitas%20Kristen%20Duta%20Wacana!5e0!3m2!1sid!2sid!4v1690455252182!5m2!1sid!2sid" width="1300" height="500" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>    
     )
